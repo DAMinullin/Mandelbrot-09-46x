3ec0add13ba177280bda10a48a6bed66b508c71a
Smak2025 <smak-80+2025@yandex.ru>
2026-03-24 13:08:27 +0300

    - исправление мелких погрешностей

--- src/ru/gr0946x/ui/MainWindow.java
+++ src/ru/gr0946x/ui/MainWindow.java
@@ -3,13 +3,18 @@ package ru.gr0946x.ui;
 import ru.gr0946x.Converter;
 import ru.gr0946x.fractals.Mandelbrot;
 import ru.gr0946x.ui.painting.FractalPainter;
-import ru.gr0946x.ui.painting.colors.ColorFunction;
-import ru.gr0946x.ui.painting.colors.ColorSchemes;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.cos;
+import static java.lang.Math.log;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.sin;
 
 public class MainWindow extends JFrame {
 
@@ -17,19 +22,19 @@ public class MainWindow extends JFrame {
     private final Mandelbrot fractal;
     private final Converter conv;
     private final FractalPainter painter;
-    private final ColorFunction[] schemes = new ColorFunction[]{
-            ColorSchemes::colorScheme1,
-            ColorSchemes::colorScheme2,
-            ColorSchemes::colorScheme3,
-            ColorSchemes::colorScheme4
-    };
 
     public MainWindow(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(900, 650));
         conv = new Converter(-2.0, 1.0, -1.0, 1.0);
         fractal = new Mandelbrot(100, 2);
-        painter = new FractalPainter(conv, fractal, schemes[(int)(Math.random()*4)]);
+        painter = new FractalPainter(conv, fractal, x -> {
+            if (x == 1f) return Color.BLACK;
+            var r = (float)abs(cos(8 * x * x));
+            var g = (float)abs(sin(5 * x));
+            var b = (float)max(0, min(log(abs(sin(10*x)*cos(4*x))+0.01), 1));
+            return new Color(r, g, b);
+        });
         mainPanel = new PaintPanel(painter);
         mainPanel.setBackground(Color.WHITE);
         mainPanel.addComponentListener(new ComponentAdapter() {
