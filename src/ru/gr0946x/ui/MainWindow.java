6980404b05e1b10cfbefdcd37b26ac4275fc4b0a
anastasaa2006-creator <anastasaa2006@gmail.com>
2026-04-24 13:33:04 +0300

    Добавлено главное меню

--- src/ru/gr0946x/ui/MainWindow.java
+++ src/ru/gr0946x/ui/MainWindow.java
@@ -17,6 +17,7 @@ public class MainWindow extends JFrame {
     private final Painter painter;
     private final Fractal mandelbrot;
     private final Converter conv;
+
     public MainWindow(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(800, 650));
@@ -25,7 +26,7 @@ public class MainWindow extends JFrame {
         painter = new FractalPainter(mandelbrot, conv, (value)->{
             if (value == 1.0) return Color.BLACK;
             var r = (float)abs(sin(5 * value));
-            var g = (float)abs(cos(8 * value) * sin (3 * value));
+            var g = (float)abs(cos(8 * value) * sin(3 * value));
             var b = (float)abs((sin(7 * value) + cos(15 * value)) / 2f);
             return new Color(r, g, b);
         });
@@ -40,7 +41,96 @@ public class MainWindow extends JFrame {
             conv.setYShape(yMin, yMax);
             mainPanel.repaint();
         });
+
+        createMenuBar();
         setContent();
+    }
+
+    private void createMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("Файл");
+
+        JMenuItem saveFracItem = new JMenuItem("Сохранить как .frac...");
+        fileMenu.add(saveFracItem);
+
+        JMenuItem openFracItem = new JMenuItem("Открыть .frac...");
+        fileMenu.add(openFracItem);
+
+        fileMenu.addSeparator();
+
+        JMenuItem saveJpgItem = new JMenuItem("Сохранить как JPG...");
+        fileMenu.add(saveJpgItem);
+
+        JMenuItem savePngItem = new JMenuItem("Сохранить как PNG...");
+        fileMenu.add(savePngItem);
+
+        fileMenu.addSeparator();
+
+        JMenuItem exitItem = new JMenuItem("Выход");
+        exitItem.addActionListener(e -> System.exit(0));
+        fileMenu.add(exitItem);
+
+        JMenu editMenu = new JMenu("Правка");
+
+        JMenuItem undoItem = new JMenuItem("Отменить (Ctrl+Z)");
+        editMenu.add(undoItem);
+
+        JMenuItem redoItem = new JMenuItem("Вернуть (Ctrl+Y)");
+        editMenu.add(redoItem);
+
+        JMenu viewMenu = new JMenu("Вид");
+
+        JMenuItem zoomInItem = new JMenuItem("Увеличить");
+        viewMenu.add(zoomInItem);
+
+        JMenuItem zoomOutItem = new JMenuItem("Уменьшить");
+        viewMenu.add(zoomOutItem);
+
+        viewMenu.addSeparator();
+
+        JMenuItem resetViewItem = new JMenuItem("Сбросить вид");
+        viewMenu.add(resetViewItem);
+
+        JMenu fractalMenu = new JMenu("Фрактал");
+
+        JMenuItem juliaItem = new JMenuItem("Показать Жюлиа по точке...");
+        fractalMenu.add(juliaItem);
+
+        fractalMenu.addSeparator();
+
+        JMenuItem colorSchemeItem = new JMenuItem("Цветовая схема");
+        fractalMenu.add(colorSchemeItem);
+
+        JMenuItem iterationsItem = new JMenuItem("Настройка итераций");
+        fractalMenu.add(iterationsItem);
+
+        JMenu tourMenu = new JMenu("Экскурсия");
+
+        JMenuItem startTourItem = new JMenuItem("Начать экскурсию...");
+        tourMenu.add(startTourItem);
+
+        JMenu helpMenu = new JMenu("Помощь");
+
+        JMenuItem aboutItem = new JMenuItem("О программе");
+        aboutItem.addActionListener(e -> showAboutDialog());
+        helpMenu.add(aboutItem);
+
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(viewMenu);
+        menuBar.add(fractalMenu);
+        menuBar.add(tourMenu);
+        menuBar.add(helpMenu);
+
+        setJMenuBar(menuBar);
+    }
+
+    private void showAboutDialog() {
+        JOptionPane.showMessageDialog(this,
+                "Mandelbrot Fractal\nВерсия 1.0\n\nРазработчики:\n• Анастасия\n• Алина\n• Анеля\n• Настя\n• Кянан\n• Каир",
+                "О программе",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 
     private void setContent(){
